236bb4f66b89d6b76e8ea1884ca04d8e5727dbae
den-duar <2021211510079@iesp.edu.br>
2022-09-16 20:00:46 -0300

    Refeito classe cargo

---
+++ Padroes proj/TEDAula3/src/TED03/Cargo.java
@@ -0,0 +1,18 @@
+package TED03;
+
+public class Cargo {
+
+    private String cargo;
+
+    public Cargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
